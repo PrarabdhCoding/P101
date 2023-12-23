1ce711f4fe51b1ef16729fd125a8e6c947731656
PrarabdhCoding <137690909+PrarabdhCoding@users.noreply.github.com>
2023-12-23 15:34:57 +0530

    Add files via upload

---
+++ chat_room.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBcNAT7vnWp1ShrgHS9aBRSh0WDcP8La7Q",
+    authDomain: "let-s-chat-web-app-ad08b.firebaseapp.com",
+    databaseURL: "https://let-s-chat-web-app-ad08b-default-rtdb.firebaseio.com",
+    projectId: "let-s-chat-web-app-ad08b",
+    storageBucket: "let-s-chat-web-app-ad08b.appspot.com",
+    messagingSenderId: "347499962479",
+    appId: "1:347499962479:web:60f65b95a3aad91c34ad9a"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
